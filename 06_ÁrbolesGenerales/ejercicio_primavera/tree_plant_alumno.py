27f897ad5b73edefcaa4285bb48dfe1d4830782f
Jaime Ramirez <jramirod30@gmail.com>
2026-04-30 16:01:47 +0200

    Código de apoyo para el ejercicio de la primavera

---
+++ 06_ÁrbolesGenerales/ejercicio_primavera/tree_plant_alumno.py
@@ -0,0 +1,37 @@
+
+from tads import IGeneralTree, LinkedGeneralTree, IPosition
+
+from flower import Flower, TColor, TShape
+
+# elements of nodes are flowers
+TreePlant = IGeneralTree[Flower]
+
+tree_plant: TreePlant = LinkedGeneralTree()
+p1: IPosition[Flower] = tree_plant.add_root(Flower(TColor.GREEN, TShape.OK))
+p2: IPosition[Flower] = tree_plant.add_child_first(p1, Flower(TColor.GREEN, TShape.OK))
+p3: IPosition[Flower] = tree_plant.add_child_last(p1, Flower(TColor.GREEN, TShape.OK))
+tree_plant.insert_sibling_before(p3, Flower(TColor.RED, TShape.OK))
+p4: IPosition[Flower] = tree_plant.add_child_first(p2, Flower(TColor.GREEN, TShape.OK))
+p6: IPosition[Flower] = tree_plant.insert_sibling_after(p4, Flower(TColor.GREEN, TShape.OK))
+tree_plant.insert_sibling_before(p6, Flower(TColor.RED, TShape.OK))
+tree_plant.add_child_first(p3, Flower(TColor.GREEN, TShape.OK))
+p8 = tree_plant.add_child_last(p3, Flower(TColor.BLUE, TShape.OK))
+p9 = tree_plant.add_child_first(p8, Flower(TColor.GREEN, TShape.BROKEN))
+
+print(tree_plant)
+
+
+def is_spring(tree: TreePlant) -> bool:
+    """
+    PRE: tree is not empty
+
+    POST: indicates if the tree plant has more than 50% of
+    its flowers in spring.
+
+    A flower is in spring if it is ok and not brown
+    """
+    pass
+
+
+def is_spring_aux(tree: TreePlant, pos: IPosition[Flower]) -> int:
+    pass
